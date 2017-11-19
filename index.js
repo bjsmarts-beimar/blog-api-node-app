f4350895f384c691609f53d4c014d94eea1a314d
Beimar Medina <bmedina@vitas.com>
2017-11-19 17:19:38 -0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,88 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Instantiating the express app
+const app = express();
+
+// Setting up bodyParser to use json and set it to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//middleware goes here
+app.use(function(req, res, next) {
+    // adding CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log('Cors has been added.');
+    
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// INstantiating the express-jwt middleware
+const jwtMW = exjwt({
+    secret: 'keyboard cat 4 ever'
+});
+
+// MOCKING DB just for test
+let users = [
+    {
+        id: 1,
+        username: 'test',
+        password: 'test'
+    },
+    {
+        id: 2,
+        username: 'test2',
+        password: 'asdf12345'
+    }
+];
+
+// LOGIN ROUTE
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    // Use your DB ORM logic here to find user and compare password
+    for (let user of users) { // I am using a simple array users which i made above
+        if (username == user.username && password == user.password /* Use your password hash checking logic here !*/) {
+            //If all credentials are correct do this
+            let token = jwt.sign({ id: user.id, username: user.username }, 'keyboard cat 4 ever', { expiresIn: 129600 }); // Sigining the token
+            res.json({
+                sucess: true,
+                err: null,
+                token
+            });
+            break;
+        }
+        else {
+            res.status(401).json({
+                sucess: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    }
+});
+
+app.get('/', jwtMW /* Using the express jwt MW here */, (req, res) => {
+    res.send('You are authenticated'); //Sending some response when authenticated
+});
+
+// Error handling 
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') { // Send the error rather than to show it on the console
+        res.status(401).send(err);
+    }
+    else {
+        next(err);
+    }
+});
+
+var port = process.env.PORT || 3001;        // set our port
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('REST API Server listen on port ' + port);
